cd8304d93ec2fed2caa9f017846b602dc0f1b0cf
MemoArguello <elmemozac6@gmail.com>
2025-09-10 15:12:48 -0300

    PWA añadido

---
+++ sw.js
@@ -0,0 +1,21 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("radio-cache").then((cache) => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/css/style.css",
+        "/js/script.js",
+        "/images/logo.png"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
